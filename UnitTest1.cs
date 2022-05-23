aaeb6da9fa217bc7207b98d029183fb42ea8f693
apshanbegam <94931797+apshanbegam@users.noreply.github.com>
2022-05-23 20:12:15 +0100

    Initial Commit

---
+++ UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+
+namespace Bowling_Kata_Csharp;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+        //Initail commit
+    }
+}
